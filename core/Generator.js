24361a123e04da1d0ca4962f23244540e38dda0e
Jun Ye <664371281@qq.com>
2022-06-21 22:45:48 +0800

    fix

--- core/Generator.js
+++ core/Generator.js
@@ -1,41 +1,99 @@
-class Generator{
-    constructor(type,parent){
-        this.type = type;
-        this.parent = parent;
-        this.line = 0;
-        this.column = 0;
-        this.token = null;
-        this.result = {};
+const events = require('events');
+class Generator extends events.EventEmitter {
+    constructor(type,module,compilation,compiler,scope,stack){
+        this._type = type;
+        this._module = module;
+        this._compilation = compilation;
+        this._compiler = compiler;
+        this._scope = scope;
+        this._stack = stack;
+        this._parent = null;
+        this._line = 0;
+        this._column = 0;
+        this._token = null;
+        this._plugin = null;
+        this._name = null;
+        this._platform = null;
     }
 
+    get type(){
+        return this._type;
+    }
+    get module(){
+        return this._module;
+    }
+    get compilation(){
+        return this._compilation;
+    }
+    get compiler(){
+        return this._compiler;
+    }
+    get scope(){
+        return this._scope;
+    }
+    get stack(){
+        return this._stack;
+    }
+    get line(){
+        return this._line;
+    }
+    get column(){
+        return this._column;
+    }
+    get token(){
+        return this._token;
+    }
+    get plugin(){
+        return this._plugin;
+    }
+    get name(){
+        return this._name;
+    }
+    get platform(){
+        return this._platform;
+    }
+    get parent(){
+        return this._parent;
+    }
     get children(){
         return this._children || (this._children=[]);
     }
 
     genLine(){
-        this.line++;
+        this._line++;
     }
 
     genColumn(){
-        this.column++;
+        this._column++;
     }
 
-    addChild( stack ){
-       const gen = new Generator(stack, this);
+    genToken(value){
+        this._token = value;
+        return this;
+    }
+
+    createInstanceByStack(stack){
+        return new Generator(stack.toString(),stack.module,stack.compilation,stack.compiler,stack.scope,stack);
+    }
+
+    createInstanceByType(type){
+        return new Generator(type,this.module,this.compilation,this.compiler,this.scope);
+    }
+
+    addChild(generatorInstance){
        const children = this.children || (this.children=[]);
-       children.push( gen );
+       children.push( generatorInstance );
        return gen;
     }
 
-    addChildAt( stack , index){
-        const gen = new Generator(stack , this);
+    addChildAt(generatorInstance, index){
         const children = this.children;
         if( index < 0 ){
             index = children.length + index;
         }else if( index > children.length ){
             index = children.length;
         }
-        children.splice(index,0,gen);
+        children.splice(index,0,generatorInstance);
         return gen;
     }
 
@@ -53,3 +111,5 @@ class Generator{
 
     }
 }
+
+module.exports = Generator;
