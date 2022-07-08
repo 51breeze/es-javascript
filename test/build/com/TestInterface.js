ca2bc6c7b308ee220880b2aada7a4bd20864196e
Jun Ye <664371281@qq.com>
2022-07-08 22:56:56 +0800

    fix

--- test/build/com/TestInterface.js
+++
@@ -1,8 +0,0 @@
-var Class = require("./../core/Class.js");
-function TestInterface(){}
-Class.creator(2,TestInterface,{
-	'id':2,
-	'ns':'com',
-	'name':'TestInterface'
-}, false);
-module.exports=TestInterface;
